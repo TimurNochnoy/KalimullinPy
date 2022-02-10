d0d744f16f6fb36f9be184b61ce3e79791caf4af
TimurNochnoy <90564869+TimurNochnoy@users.noreply.github.com>
2022-02-10 11:59:31 +0300

    Delete 08-login directory

--- 08-login/main.py
+++
@@ -1,7 +0,0 @@
-first_name = input('Введите имя пользователя: ')
-greeting = 'Привет,'
-print(greeting, first_name)
-intro = "К сожалению, у Вас нет доступа к системе."
-print(intro)
-info = "Пожалуйста, обратитесь к системному администратору."
-print(info)
